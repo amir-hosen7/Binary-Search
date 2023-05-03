9e7ad6a6884c2afa150a69c812b436e275c834d7
Amir Hosen <42311923+amir-hosen7@users.noreply.github.com>
2023-05-03 17:02:53 +0600

    Create Multiplication Table.cpp

---
+++ Search on Result/CSES/Multiplication Table.cpp
@@ -0,0 +1,75 @@
+//https://cses.fi/problemset/task/2422/
+
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef unsigned long long ull;
+#define pb push_back
+#define F first
+#define S second
+#define all(v) v.begin(), v.end()
+#define Mid(b, e) b + (e - b) / 2
+#define inf 1e18
+double PI = 2 * acos(0.0);
+
+//---------------------------------------------------------------------------//
+//Timus: 314795JS
+//knight moves
+// ll X[] = {2, 1, -1, -2, -2, -1, 1, 2};
+// ll Y[] = {1, 2, 2, 1, -1, -2, -2, -1};
+
+//8 direction
+// ll dx[] = {-1, -1, -1, 0, 0, 1, 1, 1};
+// ll dy[] = {-1, 0, 1, -1, 1, -1, 0, 1};
+
+//4 direction
+// ll dx[]={1, -1, 0, 0};
+// ll dy[]={0, 0, 1, -1};
+
+void yesNo(bool ck){cout << (ck ? "YES\n" : "NO\n");}
+//------------------------------------------------------------------------------//
+
+//ll toInt(string s){stringstream ss;ss<<s;int n;ss>>n;return n;}
+//string toString(ll n){stringstream ss;ss<<n;string s;ss>>s;return s;}
+//typedef tree<ll, null_type, less_equal<ll>, rb_tree_tag,tree_order_statistics_node_update> ordered_set;
+//typedef tree<ll, null_type, less<ll>, rb_tree_tag, tree_order_statistics_node_update> ordered_set;
+
+
+const ll P = 29;
+const ll N = 2e5 + 50;
+const ll mod = 1e9 + 7;
+
+int main() {
+
+    //freopen("input.txt", "r", stdin);
+    //freopen("output.txt", "w", stdout);
+    //ios_base::sync_with_stdio(false); cin.tie(0); cout.tie(0);
+
+    ll t = 1;
+    for (ll T = 1; T <= t; T++) {
+        ll n;
+        cin >> n;
+        ll l = 1, r = n * n;
+        ll middle = r / 2 + 1, res;
+        while (l <= r) {
+            ll mid = l + (r - l) / 2, cnt = 0;
+            for (ll i = 1; i <= n; i++) {
+                cnt += min(n, mid / i);
+            }
+            if (cnt >= middle) {
+                r = mid - 1;
+                res = mid;
+            }
+            else {
+                l = mid + 1;
+            }
+        }
+        cout << res << "\n";
+    }
+    return 0;
+}
+
+
+
+
+
